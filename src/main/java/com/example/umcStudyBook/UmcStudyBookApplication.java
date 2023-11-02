02c981e9acf5bd2e2a291857c57f5d545172b680
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-02 17:25:44 +0900

    initialization

---
+++ src/main/java/com/example/umcStudyBook/UmcStudyBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.umcStudyBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcStudyBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcStudyBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/umcStudyBook/UmcStudyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umcStudyBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcStudyBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
